2fe93036194cdeab4ad88606a87cf1808a1a4003
gsikora1028 <gabrielsikora28@gmail.com>
2023-07-18 10:43:25 -0700

    comments

--- DV2_Scrubber.py
+++ DV2_Scrubber.py
@@ -1,7 +1,16 @@
+# ********************************************************************************************
+# Program: DV2 Metric Scrubber
+# Author: Gabe Sikora
+# Date: 5/2/2023
+# Function: Concatenate DV2 Adhoc Query Files into a single .xlsx file --> Pass to Tableau
+# ********************************************************************************************
+
 import pandas as pd
 import os
 
-#DV2 Data scrubbing
+print ('Accessing DV2 Adhoc Query Files...')
+
+#List DV2 Adhoc Query filepaths
 filepaths = [
     '/Users/gsikora/Desktop/GH/5-Data_Metrics/DV/2023/1-Jan-DV2.xlsx',
     '/Users/gsikora/Desktop/GH/5-Data_Metrics/DV/2023/2-Feb-DV2.xlsx',
@@ -14,12 +23,13 @@ filepaths = [
 # Read all the Excel files into separate dataframes
 dataframes = [pd.read_excel(filepath) for filepath in filepaths]
 
+# Concatenate the dataframes into 1 master dataframe
 master_dv = pd.concat(dataframes, axis=0)
 master_dv.columns = ["Accession Number", "Project ID", "Test", "Type", "Completion Date", "User"]
 
 # Convert "Completion Date" to datetime format
 master_dv["Completion Date"] = pd.to_datetime(master_dv["Completion Date"]).dt.strftime('%m/%d/%Y')
-
-master_dv.to_excel('/Users/gsikora/Desktop/GH/5-Data_Metrics/DV/2023/Master File/master_dv2_metrics.xlsx', sheet_name='DV2 Metrics', index=False)
-
 print(master_dv)
+
+# Save master dataframe to Excel File
+master_dv.to_excel('/Users/gsikora/Desktop/GH/5-Data_Metrics/DV/2023/Master File/master_dv2_metrics.xlsx', sheet_name='DV2 Metrics', index=False)
--- Emanifest_Upload_Metric_Scrubber.py
+++ Emanifest_Upload_Metric_Scrubber.py
@@ -1,7 +1,16 @@
+# ********************************************************************************************
+# Program: E-Manifest Metric Scrubber
+# Author: Gabe Sikora
+# Date: 5/2/2023
+# Function: Concatenate EMAN Adhoc Query Files into a single .xlsx file --> Pass to Tableau
+# ********************************************************************************************
+
 import pandas as pd
 import os
 
-# Emanifest Upload metric Data scrubbing
+print ('Accessing E-Manifest Adhoc Query Files...')
+
+# List EMAN Adhoc Query filepaths
 filepaths = [
     '/Users/gsikora/Desktop/GH/5-Data_Metrics/EMAN/2023/1-Jan-EMAN.xlsx',
     '/Users/gsikora/Desktop/GH/5-Data_Metrics/EMAN/2023/2-Feb-EMAN.xlsx',
@@ -15,17 +24,13 @@ filepaths = [
 # Read all the Excel files into separate dataframes
 dataframes = [pd.read_excel(filepath) for filepath in filepaths]
 
-# Concatenate the dataframes
+# Concatenate the dataframes into 1 master dataframe
 master_dv = pd.concat(dataframes, axis=0)
 master_dv.columns = ["Accession Number", "Project ID", "User", "Completion Date"]
 
-# Convert "Completion Date" to datetime format
-# master_dv["Completion Date"] = pd.to_datetime(master_dv["Completion Date"]).dt.strftime('%m/%d/%Y')
-
 # Identify rows with unique dates
 unique_dates = master_dv.drop_duplicates(subset=['Project ID' , 'Completion Date'])
-
 print(unique_dates)
 
-# Save unique dates to Excel file
+# Save unique dates to Excel file (indicates the number of manifests uploaded)
 unique_dates.to_excel('/Users/gsikora/Desktop/GH/5-Data_Metrics/EMAN/2023/Master File/master_EMAN_metrics.xlsx', sheet_name='EMAN Metrics', index=False)
